92a5f8aaf3e1cb18c243853571aca658a3cc6e70
owlot <95369289+owlot@users.noreply.github.com>
2023-04-27 16:26:52 +0200

    style: fix code style conform prettier

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,22 +1,38 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-2 px-sm-3 px-xxl-5">
+    <nav
+        class="navbar navbar-expand-lg navbar-dark bg-dark px-2 px-sm-3 px-xxl-5"
+    >
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <div class="logo">logo</div>
             </a>
-            <button class="navbar-toggler order-0 order-sm-2 ms-0 ms-md-3" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button
+                class="navbar-toggler order-0 order-sm-2 ms-0 ms-md-3"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse order-3 order-lg-1" id="navbarNav">
+            <div
+                class="collapse navbar-collapse order-3 order-lg-1"
+                id="navbarNav"
+            >
                 <ul class="navbar-nav">
                     <li><a href="#" class="nav-link px-2">Features</a></li>
                     <li><a href="#" class="nav-link px-2">Pricing</a></li>
                     <li><a href="#" class="nav-link px-2">Industries</a></li>
                 </ul>
             </div>
-            <div class="d-flex flex-grow-1 order-1 order-lg-2 justify-content-center justify-content-md-end">
-                <button type="button" class="btn btn-info">Request a meeting</button>
+            <div
+                class="d-flex flex-grow-1 order-1 order-lg-2 justify-content-center justify-content-md-end"
+            >
+                <button type="button" class="btn btn-info">
+                    Request a meeting
+                </button>
             </div>
         </div>
     </nav>
@@ -25,12 +41,34 @@
     </div>
     <div class="container-fluid">
         <footer class="py-3 my-4">
-            <ul class="nav justify-content-center border-bottom border-top py-3 mb-3">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+            <ul
+                class="nav justify-content-center border-bottom border-top py-3 mb-3"
+            >
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-2 text-body-secondary">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-2 text-body-secondary">
+                        Features
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-2 text-body-secondary">
+                        Pricing
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-2 text-body-secondary">
+                        FAQs
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-2 text-body-secondary">
+                        About
+                    </a>
+                </li>
             </ul>
             <p class="text-center text-body-secondary">&copy; 2023 Arrow DAO</p>
         </footer>
--- src/plugins/bootstrap.client.js
+++ src/plugins/bootstrap.client.js
@@ -1,7 +1,15 @@
-import {Modal, Collapse, Accordion, Offcanvas} from 'bootstrap/dist/js/bootstrap.bundle'
+import {
+  Modal,
+  Collapse,
+  Accordion,
+  Offcanvas,
+} from 'bootstrap/dist/js/bootstrap.bundle';
 
-export default defineNuxtPlugin(nuxtApp => {
-    nuxtApp.provide('bootstrap', {
-      Modal, Collapse, Accordion, Offcanvas
-    })
-})
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.provide('bootstrap', {
+    Modal,
+    Collapse,
+    Accordion,
+    Offcanvas,
+  });
+});
